2ac9ba145eafdbb2d2e82891c15ac52555a4b145
‘JunHooo’ <2423026235@qq.com>
2022-03-11 13:52:24 +0800

    创建switch组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    Switch 的文档
+    <!--    <Switch :value="value" @update:value="value=$event"></Switch>-->
+    <h1>基本用法</h1>
+    <Switch v-model:value="value" ></Switch>
   </div>
 </template>
 
 <script lang="ts">
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
 export default {
-name: "SwitchDemo"
+name: "SwitchDemo",
+  components: {Switch},
+  setup(){
+    let value=ref(false)
+    return{value}
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="button" @click="move" :class="{check:value}" >
+    <div class="circle"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import {ref} from 'vue';
+
+export default {
+  props:{
+    value:Boolean
+  },
+  name: 'Switch',
+  setup(props,context){
+    const move=()=>{
+       context.emit('update:value',!props.value)
+    }
+    return{move}
+  }
+
+};
+</script>
+
+<style lang="scss" scoped>
+  .button{
+    height: 20px;
+    width: 40px;
+    border-radius: 20px;
+    background: rgb(255, 73, 73);
+    position: relative;
+    > .circle{
+      background: rgb(255, 255, 255);
+      height: 16px;
+      width: 16px;
+      border-radius: 50%;
+      top:2px;
+      left: 2px;
+      position: absolute;
+      transition: left 300ms;
+    }
+  }
+  .check{
+    background: rgb(19, 206, 102);
+
+    > .circle{
+      left: 21px;
+    }
+  }
+
+</style>
