4fa3ba4690ae8be5193149ee23668cf878481748
‘JunHooo’ <2423026235@qq.com>
2022-03-12 10:14:11 +0800

    基本完成Button组件。

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,8 +1,69 @@
 <template>
-  <Button>你好</Button>
-  <Button theme="button">你好</Button>
-  <Button theme="link">你好</Button>
-  <Button theme="text">你好</Button>
+  <h1>基础用法</h1>
+  <h5>基础的按钮用法</h5>
+  <div class="Sip-content">
+    <Button>默认按钮</Button>
+    <Button theme="primary">主要按钮</Button>
+    <Button theme="success">成功按钮</Button>
+    <Button theme="danger">危险按钮</Button>
+    <Button theme="link">链接按钮</Button>
+    <Button theme="text">文本按钮</Button>
+  </div>
+  <div class="Sip-content">
+    <Button theme="button-plain">朴素按钮</Button>
+    <Button theme="primary-plain">主要按钮</Button>
+    <Button theme="success-plain">成功按钮</Button>
+    <Button theme="danger-plain">危险按钮</Button>
+    <Button theme="link-plain">链接按钮</Button>
+    <Button theme="text-plain">文本按钮</Button>
+  </div>
+  <div class="Sip-content">
+    <Button theme="button" :round=true>朴素按钮</Button>
+    <Button theme="primary" :round=true>主要按钮</Button>
+    <Button theme="success" :round=true>成功按钮</Button>
+    <Button theme="danger" :round=true>危险按钮</Button>
+    <Button theme="link" :round=true>链接按钮</Button>
+    <Button theme="text" :round=true>文本按钮</Button>
+  </div>
+  <h1>禁用状态</h1>
+  <h5>按钮不可用状态</h5>
+  <div class="Sip-content one">
+    <Button :disable=true>默认按钮</Button>
+    <Button theme="primary" :disable=true>主要按钮</Button>
+    <Button theme="success" :disable=true>成功按钮</Button>
+    <Button theme="danger" :disable=true>危险按钮</Button>
+    <Button theme="link" :disable=true>链接按钮</Button>
+    <Button theme="text" :disable=true>文本按钮</Button>
+  </div>
+  <div class="Sip-content one">
+    <Button theme="button-plain" :disable=true>朴素按钮</Button>
+    <Button theme="primary-plain" :disable=true>主要按钮</Button>
+    <Button theme="success-plain" :disable=true>成功按钮</Button>
+    <Button theme="danger-plain" :disable=true>危险按钮</Button>
+    <Button theme="link-plain" :disable=true>链接按钮</Button>
+    <Button theme="text-plain" :disable=true>文本按钮</Button>
+  </div>
+  <h1>示例2</h1>
+  <div class="Sip-content">
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
+  <div class="Sip-content">
+    <Button theme="link" size="big">大大大</Button>
+    <Button theme="link">普普通</Button>
+    <Button size="small" theme="link">小小小</Button>
+  </div>
+  <div class="Sip-content">
+    <Button size="big" theme="text">大大大</Button>
+    <Button theme="text">普普通</Button>
+    <Button size="small" theme="text">小小小</Button>
+  </div>
+  <h1>示例5</h1>
+  <div>
+    <Button loading>加载中</Button>
+    <Button>加载完毕</Button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,18 +72,29 @@ import Button from '../lib/Button.vue';
 export default {
   name: 'ButtonDemo',
   components: {Button},
-  setup(){
-    const onclick=()=>{
-      console.log("hi")
-    }
-    const onmouseover=()=>{
+  setup() {
+    const onclick = () => {
+      console.log('hi');
+    };
+    const onmouseover = () => {
       console.log('hi2');
-    }
-    return {onclick,onmouseover}
+    };
+    return {onclick, onmouseover};
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
+.Sip-content {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  border: 1px solid rgb(220, 223, 230);
 
+}
+  .one {
+  background: white;
+  border: 1px solid rgb(220, 223, 230);
+
+}
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,26 +1,55 @@
 <template>
-  <div>
-    <button class="Sip-button" :class="{[`Sip-theme-${theme}`]: theme}">
+    <button class="Sip-button" :class="classes">
+      <span v-if="loading" class="Sip-loadingIndicator"></span>
       <slot></slot>
     </button>
-  </div>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   inheritAttrs: false,
   name: 'Button',
-  props:{
-    theme:{
-      type:String,
-      default:'button'
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+    disable: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
+  },
+  setup(props) {
+    const {theme, size, round, disable} = props;
+    const classes = computed(() => {
+      return {
+        [`Sip-theme-${theme}`]: theme,
+        [`Sip-size-${size}`]: size,
+        [`Sip-round-${round}`]: round,
+        [`Sip-disable-${disable}`]: disable,
+      };
+    });
+    return {classes};
   }
 };
 </script>
 
-<style lang="scss" >
-$h: 32px;
+<style lang="scss">
+$h: 40px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
@@ -28,7 +57,8 @@ $radius: 4px;
 .Sip-button {
   box-sizing: border-box;
   height: $h;
-  padding: 0 12px;
+  padding: 0 20px;
+  margin: 12px;
   cursor: pointer;
   display: inline-flex;
   justify-content: center;
@@ -39,19 +69,248 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+
   & + & {
     margin-left: 8px;
   }
+
   &:hover,
   &:focus {
     color: $blue;
     border-color: $blue;
+    background: rgb(236, 245, 255);
   }
+
   &:focus {
     outline: none;
   }
+
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.Sip-disable-true {
+    border: 1px solid rgb(220, 223, 230);
+    cursor: not-allowed;
+    color: rgb(220, 207, 204);
+    background: white;
+    &:hover,
+    &:focus {
+      color: rgb(220, 207, 204);
+      background: white;
+    }
+  }
+
+  &.Sip-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.Sip-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.Sip-theme-primary {
+    border-color: transparent;
+    box-shadow: none;
+    color: white;
+    background: rgb(102, 177, 255);
+
+    &:hover,
+    &:focus {
+      background: rgb(58, 142, 230);
+    }
+    &.Sip-disable-true {
+      cursor: not-allowed;
+      background: rgb(160, 207, 255);
+      &:hover,
+      &:focus {
+        color: white;
+        background: rgb(160, 207, 255)
+      }
+    }
+  }
+
+  &.Sip-theme-success {
+    border-color: transparent;
+    box-shadow: none;
+    color: white;
+    background: rgb(133, 206, 97);
+
+    &:hover,
+    &:focus {
+      background: rgb(93, 175, 52);
+    }
+
+    &.Sip-disable-true {
+      cursor: not-allowed;
+      background: rgb(179, 225, 157);
+      border: none;
+      &:hover,
+      &:focus {
+        color: white;
+        background: rgb(179, 225, 157)
+      }
+    }
+  }
+
+  &.Sip-theme-danger {
+    border-color: transparent;
+    box-shadow: none;
+    color: white;
+    background: rgb(247, 137, 137);
+
+    &:hover,
+    &:focus {
+      background: rgb(221, 97, 97);
+    }
+
+    &.Sip-disable-true {
+      cursor: not-allowed;
+      background: rgb(250, 182, 182);
+      &:hover,
+      &:focus {
+        color: white;
+        background: rgb(250, 182, 182)
+      }
+    }
+  }
+
+  &.Sip-theme-button-plain {
+    &:hover,
+    &:focus {
+      background: white;
+      color: rgb(58, 142, 230);
+    }
+    &.Sip-disable-true {
+      border: 1px solid rgb(220, 223, 230);
+      cursor: not-allowed;
+      color: rgb(220, 207, 204);
+      background: white;
+      &:hover,
+      &:focus {
+        color: rgb(220, 207, 204);
+        background: white;
+      }
+    }
+  }
+
+  &.Sip-theme-primary-plain {
+    border-color: transparent;
+    box-shadow: none;
+    color: rgb(58, 142, 230);
+    background: rgb(236, 245, 255);
+
+    &:hover,
+    &:focus {
+      background: rgb(58, 142, 230);
+      color: white;
+    }
+    &.Sip-disable-true {
+      border: 1px solid rgb(220, 223, 230);
+      color: rgb(58, 142, 230);
+      cursor: not-allowed;
+      background: rgb(236, 245, 255);
+      &:hover,
+      &:focus {
+        color: rgb(58, 142, 230);
+        background: rgb(236, 245, 255)
+      }
+    }
+  }
+
+  &.Sip-theme-success-plain {
+    border-color: transparent;
+    box-shadow: none;
+    color: rgb(93, 175, 52);
+    background: rgb(240, 249, 235);
+
+    &:hover,
+    &:focus {
+      color: white;
+      background: rgb(93, 175, 52);
+    }
+    &.Sip-disable-true {
+      border: 1px solid rgb(220, 223, 230);
+      color: rgb(93, 175, 52);
+      cursor: not-allowed;
+      background: rgb(240, 249, 235);
+      &:hover,
+      &:focus {
+        color: rgb(93, 175, 52);
+        background: rgb(240, 249, 235)
+      }
+    }
+  }
+
+  &.Sip-theme-danger-plain {
+    border-color: transparent;
+    box-shadow: none;
+    color: rgb(221, 97, 97);
+    background: rgb(254, 240, 240);
+
+    &:hover,
+    &:focus {
+      background: rgb(221, 97, 97);
+      color: white;
+    }
+    &.Sip-disable-true {
+      border: 1px solid rgb(220, 223, 230);
+      color: rgb(221, 97, 97);
+      cursor: not-allowed;
+      background: rgb(254, 240, 240);
+      &:hover,
+      &:focus {
+        color: rgb(221, 97, 97);
+        background: rgb(254, 240, 240)
+      }
+    }
+  }
+
+  &.Sip-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.Sip-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.Sip-round-true {
+    border-radius: 20px;
+  }
+
+  > .Sip-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: Sip-spin 1s infinite linear;
+  }
+}
+@keyframes Sip-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
+
 }
 </style>
