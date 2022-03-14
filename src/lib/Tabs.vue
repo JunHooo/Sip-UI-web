52391cb1bb50654ca1d83e2939d6ff1a68c50d88
‘JunHooo’ <2423026235@qq.com>
2022-03-14 09:55:04 +0800

    优化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="Sip-tabs">
     <div class="Sip-tabs-nav" ref="container">
-      <div class="Sip-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (el) navItems[index] = el }"
-           @click="select(t)" :class="{selected: t=== selected}" :key="index">{{ t }}
-      </div>
+      <div class="Sip-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
       <div class="Sip-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="Sip-tabs-content">
-      <component class="Sip-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults"
-                 :is="c"/>
+      <component class="Sip-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
     </div>
   </div>
 </template>
-
 <script lang="ts">
-import Tab from './Tab.vue';
-import {computed, ref, onMounted, onUpdated} from 'vue';
-
+import Tab from './Tab.vue'
+import {
+  computed,
+  ref,
+  onMounted,
+  watchEffect
+} from 'vue'
 export default {
   props: {
     selected: {
@@ -24,59 +24,48 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref < HTMLDivElement[] > ([])
+    const selectedItem = ref < HTMLDivElement > (null)
     const indicator = ref < HTMLDivElement > (null)
     const container = ref < HTMLDivElement > (null)
-    const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      console.log(result)
-      const {
-        width
-      } = result.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {
-        left: left1
-      } = container.value.getBoundingClientRect()
-      const {
-        left: left2
-      } = result.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
-    const defaults = context.slots.default();
+    onMounted(()=>{
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
+    const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
-        throw new Error('Tabs 子标签必须是 Tab');
+        throw new Error('Tabs 子标签必须是 Tab')
       }
-    });
-    const current = computed(() => {
-      console.log('重新 return');
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected;
-      })[0];
-    });
+    })
     const titles = defaults.map((tag) => {
-      return tag.props.title;
-    });
+      return tag.props.title
+    })
     const select = (title: string) => {
-      context.emit('update:selected', title);
-    };
+      context.emit('update:selected', title)
+    }
     return {
       defaults,
       titles,
-      current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container
-    };
+    }
   }
-};
+}
 </script>
-
 <style lang="scss">
 $blue: #40a9ff;
 $color: #333;
@@ -87,21 +76,17 @@ $border-color: #d9d9d9;
     color: $color;
     border-bottom: 1px solid $border-color;
     position: relative;
-
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
-
       &:first-child {
         margin-left: 0;
       }
-
       &.selected {
         color: $blue;
       }
     }
-
     &-indicator {
       position: absolute;
       height: 3px;
@@ -112,13 +97,10 @@ $border-color: #d9d9d9;
       transition: all 250ms;
     }
   }
-
   &-content {
     padding: 8px 0;
-
     &-item {
       display: none;
-
       &.selected {
         display: block;
       }
