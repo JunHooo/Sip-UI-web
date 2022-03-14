97900a8538fb45ec23af371846479cf55b78dd03
‘JunHooo’ <2423026235@qq.com>
2022-03-14 09:32:49 +0800

    切换显示的内容。

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="Sip-tabs">
+    <div class="Sip-tabs-nav">
+      <div class="Sip-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}"
+           :key="index">{{ t }}
+      </div>
+    </div>
+    <div class="Sip-tabs-content">
+      {{ current }}
+      <component class="Sip-tabs-content-item" :is="current"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Tab from './Tab.vue';
+import {computed} from 'vue';
+
+export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab');
+      }
+    });
+    const current = computed(() => {
+      console.log('重新 return');
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
+    return {
+      defaults,
+      titles,
+      current,
+      select
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.Sip-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
