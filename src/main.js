57640f4119ca7666b807a891ef07b26caa6a2e43
‘JunHooo’ <2423026235@qq.com>
2022-03-15 10:22:36 +0800

    优化markdown文件

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import './lib/Sip.scss'
 import './index.css'
 import {createApp} from 'vue'
 import App from './App.vue'
-import { router } from './router.ts';
+import {router} from './router.ts'
 import 'github-markdown-css'
+import Markdown from './components/Markdown.vue'
 
-const app=createApp(App)
-  app.use(router)
-  app.mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+app.component('Markdown', Markdown)
